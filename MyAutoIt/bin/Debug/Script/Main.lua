917bbbc9a0abb23c1eb178a5d24e6a3eb388fbe7
paweenwich <paweenwich@yahoo.com>
2018-03-06 14:02:02 +0700

    Async

--- MyAutoIt/bin/Debug/Script/Main.lua
+++ MyAutoIt/bin/Debug/Script/Main.lua
@@ -5,6 +5,16 @@ function ClickAt(tab)
     bot:ClickAt(tab.x,tab.y);
 end;
 
+function onDungeonClear(tab)
+    bot:ActiveBlueStackWindow();
+    bot:ClickAt(tab.x,tab.y);
+    bot:AddTask("bot:log('onDungeonClear')",3000);
+end;
+
+function onQuestItemDone(tab)
+    
+end;
+
 function ProcessTask()
     local now = Environment.TickCount;
     --bot:log();
@@ -13,14 +23,14 @@ function ProcessTask()
         local item = bot.lstTask.Items[i];
         if now > item.execTime then
             local f = loadstring(item.script);
-            f();
-            bot.lstTask.Items:RemoveAt(i);
+            if f() ~= true then
+                bot.lstTask.Items:RemoveAt(i);
+            end;
             return true
         end;
     end;
     return false;
 end;
-
 
 function Auto()
     if bot.chkAutoClick.Checked == true then
@@ -67,6 +77,12 @@ stateTable["Auto"] = {
     {screen="QuestSelectB3",cmd=ClickAt,x=700,y=222},
     {screen="QuestSelected",cmd=ClickAt,x=850,y=600},
     {screen="QuestConfirm",cmd=ClickAt,x=760,y=500},
+    {screen="ClearDungeon",cmd=onDungeonClear,x=956,y=635},
+    {screen="DungeonEXP/NoMore",cmd=ClickAt,x=1240,y=40},   -- Exit
+    {screen="DungeonGold/CanDo",cmd=ClickAt,x=1080,y=640},   
+    
+
+
     
     {screen="WeeklyQuest/NoQuest",cmd=ClickAt,x=1238,y=45},
     {screen="WeeklyQuest/DoQuest",cmd=ClickAt,x=940,y=420},
@@ -79,6 +95,43 @@ stateTable["Auto"] = {
     {screen="Main/HarvestDone",cmd=ClickAt,x=863,y=84},
     {screen="Main/PartyAutoNoSkill",cmd=ClickAt,x=874,y=683},
     {screen="Main/Quest2",cmd=ClickAt,x=52,y=312},
+};
+
+stateTable["AutoQuest"] = {
+    {screen="QuestSkip",cmd=ClickAt,x=1180,y=510},
+    {screen="QuestComplete",cmd=ClickAt,x=634,y=615},
+    {screen="QuestDo",cmd=ClickAt,x=763,y=613},
+    {screen="QuestAccept",cmd=ClickAt,x=763,y=613},
+    {screen="Move",cmd=ClickAt,x=511,y=519},
+    {screen="Dead",cmd=ClickAt,x=1199,y=252},
+    {screen="PartyRequest",cmd=ClickAt,x=900,y=56},
+    {screen="HarvestExit",cmd=ClickAt,x=760,y=482},
+    {screen="QuestSelectB2",cmd=ClickAt,x=564,y=222},
+    {screen="QuestSelectB3",cmd=ClickAt,x=700,y=222},
+    {screen="QuestSelected",cmd=ClickAt,x=850,y=600},
+    {screen="QuestConfirm",cmd=ClickAt,x=760,y=500},
+    {screen="ClearDungeon",cmd=onDungeonClear,x=956,y=635},
+    {screen="DungeonEXP/NoMore",cmd=ClickAt,x=1240,y=40},   -- Exit
+    {screen="DungeonGold/CanDo",cmd=ClickAt,x=1080,y=640},   
+    
+
+
+    
+    {screen="WeeklyQuest/NoQuest",cmd=ClickAt,x=1238,y=45},
+    {screen="WeeklyQuest/DoQuest",cmd=ClickAt,x=940,y=420},
+    {screen="WeeklyQuest/QuestSuccess",cmd=ClickAt,x=937,y=423},
+    {screen="WeeklyQuest/MoveNow",cmd=ClickAt,x=940,y=420},
+    {screen="WeeklyQuest/NoQuest",cmd=ClickAt,x=1242,y=40},
+    
+    {screen="Main/Success",cmd=ClickAt,x=260,y=330},
+    {screen="Main/Harvest",cmd=ClickAt,x=689,y=262},
+    {screen="Main/HarvestDone",cmd=ClickAt,x=863,y=84},
+    {screen="Main/PartyAutoNoSkill",cmd=ClickAt,x=874,y=683},
+    {screen="Main/Quest2",cmd=ClickAt,x=52,y=312},
+    
+    {screen="Bag",cmd=ClickAt,x=1145,y=130},
+    {screen="Bag/item3",cmd=ClickAt,x=833,y=471},
+    {screen="Bag/questItem",cmd=ClickAt,x=705,y=397},
 };
 currentState = "Auto";
 
